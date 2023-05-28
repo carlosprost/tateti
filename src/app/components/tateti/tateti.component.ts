8316962220503ec1b406a0e01880c965068f1d91
carlosaprost <carlosaprost@gmail.com>
2023-05-28 00:12:34 -0300

    juego mejorado

--- src/app/components/tateti/tateti.component.ts
+++ src/app/components/tateti/tateti.component.ts
@@ -23,16 +23,39 @@ export class TatetiComponent {
   empate: boolean = false;
 
   mensaje: string = '';
+  constructor() {
+    this.ia();
+  }
 
-  
+  ia() {
+    let aiEspera = setInterval(() => {
+      if (!this.X) {
+        setTimeout(() => {
+          this.comprobarJugadaGanadora();
+          let casillas = document.querySelectorAll('.casilla');
+          let casillaVacia = Array.from(casillas).filter(
+            (v) => v.innerHTML == ''
+          );
+          let casilla = casillaVacia[
+            Math.floor(Math.random() * casillaVacia.length)
+          ] as HTMLDivElement;
+          this.colocarPieza(casilla);
+          this.cambiarTurno();
+          
+        }, 500);
+      }
+    }, 1000);
+  }
+
   llenarCasilla(event: Event) {
     let casilla = event.target as HTMLDivElement;
 
-    if (casilla.innerHTML == '') {
+    if (this.X && casilla.innerHTML == '') {
+      this.comprobarJugadaGanadora();
       this.colocarPieza(casilla);
       this.cambiarTurno();
     }
-    this.comprobarJugadaGanadora();
+    
   }
 
   colocarPieza(casilla: HTMLDivElement) {
@@ -68,13 +91,13 @@ export class TatetiComponent {
         this.win = true;
         this.mensaje = 'GANADOR';
         this.start = false;
-        this.ponerRaya(p)
-      }else if(this.comprobarCasillas(p) == 'complete'){
+        this.ponerRaya(p);
+      } else if (this.comprobarCasillas(p) == 'complete') {
         completeAll++;
       }
     }
 
-    if(completeAll == 8){
+    if (completeAll == 8) {
       this.empate = true;
       this.mensaje = 'EMPATE';
       this.start = false;
@@ -85,117 +108,125 @@ export class TatetiComponent {
     let pieza = casillas[0].innerHTML;
     let result: string = '';
     if (casillas.every((v) => v.innerHTML != '' && v.innerHTML === pieza)) {
-      result = 'win'
-    }else if(casillas.every((v) => v.innerHTML != '')){
-      result = 'complete'
+      result = 'win';
+    } else if (casillas.every((v) => v.innerHTML != '')) {
+      result = 'complete';
     }
-    
+
     return result;
   }
 
-  ponerRaya(casillas: Element[]){
+  ponerRaya(casillas: Element[]) {
     let col = this.columna(casillas);
     let fil = this.fila(casillas);
     let diag = this.diagonal(casillas);
-    console.log(col, fil, diag);
-    
+
     let rayaGanador = document.querySelector('#rayas') as HTMLDivElement;
     rayaGanador.parentElement?.classList.remove('invisible');
-      console.log(rayaGanador);
-    
 
-    if(col != ''){
+    if (col != '') {
       rayaGanador.classList.add(col);
-      console.log(rayaGanador.classList);
-      
-    }else if(fil != ''){
+    } else if (fil != '') {
       rayaGanador.classList.add(fil);
-      console.log(rayaGanador.classList);
-    }else if(diag != ''){
+    } else if (diag != '') {
       rayaGanador.classList.add(diag);
-      console.log(rayaGanador.classList);
     }
   }
 
-  columna(casillas: Element[]){
+  columna(casillas: Element[]) {
     let col: string = '';
-    if(this.esColumna1(casillas)){
+    if (this.esColumna1(casillas)) {
       col = 'col1';
-    }else if(this.esColumna2(casillas)){
+    } else if (this.esColumna2(casillas)) {
       col = 'col2';
-    }else if(this.esColumna3(casillas)){
+    } else if (this.esColumna3(casillas)) {
       col = 'col3';
     }
-    
+
     return col;
   }
 
-  fila(casillas: Element[]){
+  fila(casillas: Element[]) {
     let fil: string = '';
-    if(this.esFila1(casillas)){
+    if (this.esFila1(casillas)) {
       fil = 'row1';
-    }else if(this.esFila2(casillas)){
+    } else if (this.esFila2(casillas)) {
       fil = 'row2';
-    }else if(this.esFila3(casillas)){
+    } else if (this.esFila3(casillas)) {
       fil = 'row3';
     }
     return fil;
   }
 
-  diagonal(casillas: Element[]){
+  diagonal(casillas: Element[]) {
     let diag: string = '';
-    if(this.esDiagonal1(casillas)){
+    if (this.esDiagonal1(casillas)) {
       diag = 'diag1';
-    }else if(this.esDiagonal2(casillas)){
+    } else if (this.esDiagonal2(casillas)) {
       diag = 'diag2';
     }
     return diag;
   }
 
-  esColumna1(casillas: Element[]){
-    
-    return casillas[0].id == '0' && casillas[1].id == '3' && casillas[2].id == '6';
+  esColumna1(casillas: Element[]) {
+    return (
+      casillas[0].id == '0' && casillas[1].id == '3' && casillas[2].id == '6'
+    );
   }
 
-  esColumna2(casillas: Element[]){
-    return casillas[0].id == '1' && casillas[1].id == '4' && casillas[2].id == '7';
+  esColumna2(casillas: Element[]) {
+    return (
+      casillas[0].id == '1' && casillas[1].id == '4' && casillas[2].id == '7'
+    );
   }
 
-  esColumna3(casillas: Element[]){
-    return casillas[0].id == '2' && casillas[1].id == '5' && casillas[2].id == '8';
+  esColumna3(casillas: Element[]) {
+    return (
+      casillas[0].id == '2' && casillas[1].id == '5' && casillas[2].id == '8'
+    );
   }
 
-  esDiagonal1(casillas: Element[]){
-    return casillas[0].id == '2' && casillas[1].id == '4' && casillas[2].id == '6';
+  esDiagonal1(casillas: Element[]) {
+    return (
+      casillas[0].id == '2' && casillas[1].id == '4' && casillas[2].id == '6'
+    );
   }
 
-  esDiagonal2(casillas: Element[]){
-    return casillas[0].id == '0' && casillas[1].id == '4' && casillas[2].id == '8';
+  esDiagonal2(casillas: Element[]) {
+    return (
+      casillas[0].id == '0' && casillas[1].id == '4' && casillas[2].id == '8'
+    );
   }
 
-  esFila1(casillas: Element[]){
-    return casillas[0].id == '0' && casillas[1].id == '1' && casillas[2].id == '2';
+  esFila1(casillas: Element[]) {
+    return (
+      casillas[0].id == '0' && casillas[1].id == '1' && casillas[2].id == '2'
+    );
   }
 
-  esFila2(casillas: Element[]){
-    return casillas[0].id == '3' && casillas[1].id == '4' && casillas[2].id == '5';
+  esFila2(casillas: Element[]) {
+    return (
+      casillas[0].id == '3' && casillas[1].id == '4' && casillas[2].id == '5'
+    );
   }
 
-  esFila3(casillas: Element[]){
-    return casillas[0].id == '6' && casillas[1].id == '7' && casillas[2].id == '8';
+  esFila3(casillas: Element[]) {
+    return (
+      casillas[0].id == '6' && casillas[1].id == '7' && casillas[2].id == '8'
+    );
   }
 
-  reiniciarJuego(){
+  reiniciarJuego() {
     let tablero = document.querySelector('.tablero');
     let casillas = document.querySelectorAll('.casilla');
 
     tablero?.classList.remove('ganador');
-    this.reiniciarCasillas(casillas);    
+    this.reiniciarCasillas(casillas);
     this.reiniciarVariables();
     this.reiniciarRaya();
   }
 
-  reiniciarCasillas(casillas: NodeListOf<Element>){
+  reiniciarCasillas(casillas: NodeListOf<Element>) {
     casillas.forEach((casilla) => {
       casilla.innerHTML = '';
       casilla.classList.remove('X');
@@ -203,13 +234,13 @@ export class TatetiComponent {
     });
   }
 
-  reiniciarVariables(){
+  reiniciarVariables() {
     this.start = true;
     this.win = false;
     this.empate = false;
   }
 
-  reiniciarRaya(){
+  reiniciarRaya() {
     let rayaGanador = document.querySelector('#rayas') as HTMLDivElement;
     rayaGanador.parentElement?.classList.add('invisible');
     rayaGanador.classList.remove('col1');
@@ -220,5 +251,7 @@ export class TatetiComponent {
     rayaGanador.classList.remove('row3');
     rayaGanador.classList.remove('diag1');
     rayaGanador.classList.remove('diag2');
+
+    this.X = true;
   }
 }
